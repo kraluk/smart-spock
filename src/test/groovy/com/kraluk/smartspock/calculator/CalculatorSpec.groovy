48973c4a06cd8af18bf7c8cb817b2db52c808bb3
Lukasz Krauzowicz <kraluk@gmail.com>
2017-05-09 23:01:16 +0200

    Added examples written during the SFI presentation.

--- src/test/groovy/com/kraluk/smartspock/calculator/CalculatorSpec.groovy
+++ src/test/groovy/com/kraluk/smartspock/calculator/CalculatorSpec.groovy
@@ -5,5 +5,35 @@ import spock.lang.Unroll
 
 class CalculatorSpec extends Specification {
 
+    @Unroll
+    def "should add #a + #b = #c"() {
+        given:
+            def calculator = new Calculator(a, b)
+        when:
+            def result = calculator.add()
 
+        then:
+            result == c as double
+
+        where:
+            a   || b   || c
+            2.0 || 1.0 || 3.0
+            2.3 || 2.0 || 4.3
+    }
+
+    def "should add a / b = c"() {
+        given: "given"
+            def calculator = new Calculator(a, b)
+        when:
+            def result = calculator.divide()
+
+        then:
+            def e = thrown(Exception)
+        and:
+            e.cause == null && e.message == null
+
+        where:
+            a || b || c
+            1 || 0 || _
+    }
 }
--- src/test/groovy/com/kraluk/smartspock/calculator/ComplexCalculatorSpec.groovy
+++ src/test/groovy/com/kraluk/smartspock/calculator/ComplexCalculatorSpec.groovy
@@ -1,9 +1,6 @@
 package com.kraluk.smartspock.calculator
 
 import spock.lang.Specification
-import spock.lang.Unroll
-
-import static com.kraluk.smartspock.calculator.ComplexCalculator.Complex
 
 class ComplexCalculatorSpec extends Specification {
 
